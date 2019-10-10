05dea53175f86465b849fab4e3769615772d51eb
Serg <mcgorky@gmail.com>
2019-10-11 00:26:09 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  const splitStr = str.split('');
+
+  const openingBrackets = bracketsConfig.reduce((result, item) => {
+    result.push(item[0]);
+
+    return result;
+  }, []);
+
+  const closingBrackets = bracketsConfig.reduce((result, item) => {
+    result.push(item[1]);
+
+    return result;
+  }, []);
+
+  let isCorrect = true;
+
+  const processedOpeningBrackets = [];
+
+  splitStr.forEach(bracket => {
+    if (openingBrackets.includes(bracket) && (!closingBrackets.includes(bracket) || (closingBrackets.includes(bracket) && !processedOpeningBrackets.includes(bracket)))) {
+      processedOpeningBrackets.push(bracket);
+    } else {
+      const openedBrackets = bracketsConfig.find(config => config.includes(bracket))[0];
+
+      if (processedOpeningBrackets[processedOpeningBrackets.length - 1] === openedBrackets) {
+        processedOpeningBrackets.pop();
+      } else {
+        isCorrect = false;
+      }
+    }
+  });
+
+  return isCorrect;
 }
